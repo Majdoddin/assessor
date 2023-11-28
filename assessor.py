beb47c2667d55e1a41df2c4165684c2b50870a34
Ruhollah Majdoddin <r.majdodin@gmail.com>
2023-11-28 12:05:55 +0100

    trained to increase num of active vars (to 2). length is still rewarded, hopefully to promote exploration. TODO: reward depth of original formula

--- assessor.py
+++ assessor.py
@@ -159,7 +159,10 @@ def sample_sequence(model, length, start_token_id, temperature=1.0, top_k=None, 
     return detached_tokens, generated_logits
 
 # Parameters for the model
-vocab_size = 8# This should be the size of your vocabulary
+
+token_texts = {0:"ST", 1:"ET",  2:"and", 3:"or", 4:"not", 5:"x1", 6:"x2", 7:"x3", 8:"x4", 9:"x5"}
+vocab_size = len(token_texts)  # Vocabulary size of the model
+
 d_model = 512  # The dimension of the embeddings
 nhead = 16  # Number of heads in the multi-head attention mechanisms
 num_layers = 8  # Number of decoder layers
@@ -171,18 +174,20 @@ model = Transformer(vocab_size=vocab_size, d_model=d_model, nhead=nhead, num_lay
 # Parameters for inference
 start_token_id = 0  # Assuming 1 is the ID for the start token in your vocabulary
 end_token_id = 1
-token_texts = {0:"ST", 1:"ET",  2:"and", 3:"or", 4:"not", 5:"X1", 6:"X2", 7:"X3"}
 temperature = 1 #0.7  # near 0 makes more deterministic
 top_k = None # Top-k filtering, should be less than the vocabulary size
-vocab_size = 10  # Vocabulary size of the model
 
 optimizer = optim.Adam(model.parameters(), lr=0.000001)
+checkpoint = torch.load('state-2800.pt')
+model.load_state_dict(checkpoint['model_state_dict'])
+optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
+
 model.train()
 
 #model.eval()
 etw = 1.0
 last_hit = 200
-for i in range(5000):
+for i in range(2816, 5000):
     optimizer.zero_grad()
     # if i > 299 and (i % 100 == 0):
     #         temperature /= 1.03
@@ -196,7 +201,7 @@ for i in range(5000):
     else:
         valid = 1. if is_polish_normal_form([token_texts[t.item()] for t in tokens[1:-1]]) else 0.
 
-    print(f"{i} " + " ".join([token_texts[t.item()] for t in tokens]))
+    #print(f"{i} " + " ".join([token_texts[t.item()] for t in tokens]))
     tknst = [token_texts[t.item()] for t in tokens[1:-1]]
     # if valid > 0.5 and i > 200:
     #     print(f"{i} " + " ".join([token_texts[t.item()] for t in tokens]))
@@ -212,12 +217,29 @@ for i in range(5000):
         exp1 = algebra.parse(nl, simplify=False)
         exp1 = exp1.simplify()
         exp1 = exp1.simplify()
-        varn2 = len(exp1.symbols())
+        varn2 = len(exp1.symbols)
+        #add demorgan again if needed
+        # if (i > 1000 and varn2 == 0) or (i > 2000 and varn2 ==1):
+        #     w = 0
+        #w = (i - last_hit) * max(varn2 - ((i-200)/100)**0.4, 0)
+        w = (i - last_hit)
+        w *= max(tokens.shape[0] - 2 - ((i-200)/100)**0.5, 1)
+        if (i > 500 and len(tknst) == 1) or (i > 600 and len(tknst) == 2 and tknst[0] == 'not'):
+            w = 0.
+        if (i > 1000 and varn2 == 0):
+            w *= max(0, (1 - (i-1000)/1000))
+        if (i > 2000 and varn2 == 1):
+            w *= max(0, (1 - (i-2000)/1000))
+        if (i > 3000 and varn2 == 2):
+            w *= max(0, (1 - (i-3000)/1000))
+        if (i < 2000 and varn2 >= 2) or (i < 3000 and varn2 >= 3) or (i > 3000 and varn2 >= 4):
+            w *= varn2**2
 
-        w = (i - last_hit) * max(varn2 - ((i-200)/100)**0.5, 0)
         if w == 0:
             continue
         last_hit = i
+        print(f"{i} " + " ".join([token_texts[t.item()] for t in tokens]))
+        print(f"{varn2} {exp1}")
 
     weight = torch.full(tokens_logs.shape, w)
     loss = binary_cross_entropy_with_logits(weight=weight, input=tokens_logs, target=target)
@@ -230,3 +252,9 @@ torch.save(model.state_dict(), "state_dict-1.pt")
 # model.eval()
 # tokens_logs = sample_sequence(model, max_length, start_token_id, temperature, top_k, vocab_size)
 # print([token_texts[t] for t in tokens_logs])
+
+
+torch.save({
+            'model_state_dict': model.state_dict(),
+            'optimizer_state_dict': optimizer.state_dict(),
+            }, 'state-5000.pt')
